7f8b14df4146097632988b33161aea9288bd23f4
Rishabh Rathore <rishabh.rathore@w3villa.com>
2025-06-26 11:44:36 +0530

    gallery

--- Calculator/Screen/otpvalidation.jsx
+++ Calculator/Screen/otpvalidation.jsx
@@ -1,79 +1,21 @@
-import { View, StyleSheet, TextInput , TouchableOpacity , Alert } from 'react-native';
-import React, { useContext, useRef, useState } from 'react';
-import { Text } from 'react-native-gesture-handler';
-import {Colors} from '../utils/Colors'
-import { ThemeContext } from '../Context/ThemeContext';
+
+In Gemfile:
+  rails was resolved to 6.1.7.10, which depends on
+    railties was resolved to 6.1.7.10, which depends on
+      rake
+w3villa@w3villa-Inspiron-5583:~/Desktop/project/app$ 
+  /usr/local/bin/bundle:25:in `<main>'
+
+An error occurred while installing rake (13.3.0), and Bundler cannot continue.
+
+In Gemfile:
+  rails was resolved to 6.1.7.10, which depends on
+    railties was resolved to 6.1.7.10, which depends on
+      rake
+w3villa@w3villa-Inspiron-5583:~/Desktop/project/app$ 
 
 
 
-const OtpValidation = ({navigation}) => {
-
-  const { currentTheme} = useContext(ThemeContext);
-
-  const [OTP, setOTP] = useState(new Array(6).fill(''));
-  const inputRef = useRef([]);
-
-  const handleChange = (text, index) => {
-    if (/^\d$/.test(text)) {
-      const newOtp = [...OTP];
-      newOtp[index] = text;
-      setOTP(newOtp);
-
-      if (index < 5) {
-        inputRef.current[index + 1].focus();
-      }
-    }
-  };
-
-  const handleKeyPress = ({ nativeEvent }, index) => {
-    if (nativeEvent.key === 'Backspace') {
-      if (OTP[index] === '') {
-        if (index > 0) {
-          inputRef.current[index - 1].focus();
-        }
-      } else {
-        const newOtp = [...OTP];
-        newOtp[index] = '';
-        setOTP(newOtp);
-      }
-    }
-  };
-
-
-   const handleSubmit = () => {
-    const enteredOtp = OTP.join('');
-    if (enteredOtp === '111111') {
-      navigation.navigate('Mainscreen');
-    } else {
-      Alert.alert('Invalid OTP', 'Please enter a valid OTP (try 111111)');
-      setOTP(new Array(6).fill(''));
-inputRef.current[0].focus();
-
-    }
-  };
-
-  return (
-    <>
-    <View style={[styles.wrraper , {backgroundColor : currentTheme === 'dark' ? Colors.dark : Colors.light} ]}>
-    <View style={styles.container}>
-      {OTP.map((digit, index) => (
-        <TextInput
-          key={index}
-          style={styles.input}
-          keyboardType="number-pad"
-          maxLength={1}
-          value={digit}
-          onChangeText={(text) => handleChange(text, index)}
-          onKeyPress={(e) => handleKeyPress(e, index)}
-          ref={(ref) => (inputRef.current[index] = ref)}
-        />
-      ))}
-    </View>
-        <TouchableOpacity style={[styles.button , {backgroundColor : currentTheme === 'dark' ? Colors.btnDarks  : Colors.btnLight}]} onPress={handleSubmit}>
-            <Text style={[styles.buttonText , { color : currentTheme === 'dark' ? "white" : "black"}]}>Submit</Text>
-        </TouchableOpacity>
-    </View>
-    </>
   );
 };
 
